0c8e96dec80e663d7d2a1c08cd323c69d7e0efe9
HenriqueMitsuo <henrique.informatica123@gmail.com>
2020-04-17 15:46:28 -0300

    Adicionado configuração do linter

---
+++ src/components/EventCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1></h1>
+    <ul>
+      <li></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quote: 'I want to be single'
+    }
+  }
+}
+</script>
+
+<style></style>
