a80d82341c5d692193dbff3597857c6aca243dab
HenriqueMitsuo <henrique.informatica123@gmail.com>
2020-04-18 16:28:00 -0300

    Adicionado Vuex parte 1

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,68 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import EventService from '../services/EventService.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    user: { id: 'abc123', name: 'John Doe' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false },
+      { id: 3, text: '...', done: true },
+      { id: 4, text: '...', done: false }
+    ],
+    events: [
+      { id: 1, title: '...', organizer: '...' },
+      { id: 2, title: '...', organizer: '...' },
+      { id: 3, title: '...', organizer: '...' },
+      { id: 4, title: '...', organizer: '...' }
+    ]
+  },
+  //? MUTATIONS - Funciona com um setter, sendo a forma principal de modificar o state
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    }
+  },
+  //? ACTIONS - Executa o processamento da informação e executa/commit por meio das mutations
+  actions: {
+    createEvent({ commit }, event) {
+      return EventService.postEvent(event).then(() => {
+        commit('ADD_EVENT', event)
+      })
+    }
+  },
+  //? GETTERS - São utilizados para requisitar informação do state de forma global(de qualquer parte do projeto)
+  getters: {
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    },
+    // getEventById = function getEventById(state) {
+    //   return function (id) {
+    //     return state.events.find(function (event) {
+    //       return event.id === id;
+    //     });
+    //   };
+    // };
+    catLength: state => {
+      return state.categories.length
+    },
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    activeTodosCount: (state, getters) => {
+      return state.todos.length - getters.doneTodos.length
+    }
+  }
 })
---
+++ src/views/EventCreate2.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.id }}</p>
+    <p>There are {{ catLength }} categories</p>
+    <p>{{ getEventById(1) }}</p>
+    <ul>
+      <li v-for="(categorie, index) in categories" :key="index">
+        {{ categorie }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters(['catLength', 'getEventById']),
+    //? Shorthand e Spread para poder usar Computed State e Computed Local
+    ...mapState(['user', 'categories'])
+  }
+  //? Formas de nomear os states
+  //* computed: mapState({
+  //*   userName: state => state.user.name,
+  //*   userId: state => state.user.id,
+  //!   ---------------------------------
+  //*   user: 'user',
+  //*   categories: 'categories'
+  //* })
+}
+</script>
+
+<style></style>
