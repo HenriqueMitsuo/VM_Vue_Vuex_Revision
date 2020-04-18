084fde9ae068edcba7eaef989aa2bf18bd46a180
HenriqueMitsuo <henrique.informatica123@gmail.com>
2020-04-18 14:35:16 -0300

    Finalizado curso

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -6,26 +6,15 @@
     <div class="event-card -shadow">
       <span class="eyebrown">@{{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
-      <span>{{ event.attendees.length }} attending</span>
+      <BaseIcon name="users">{{ event.attendees.length }} attending</BaseIcon>
     </div>
   </router-link>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      event: {
-        id: 1,
-        title: 'Beach Cleanup',
-        date: 'Tues Aug 19, 2018',
-        time: '6:00',
-        attendees: [
-          { id: 'abc123', name: 'John Doe' },
-          { id: '123abc', name: 'Jane Doe' }
-        ]
-      }
-    }
+  props: {
+    event: Object
   }
 }
 </script>
